1cef79d34011b9f4949b6ce873064c68a3597143
Erik Fonselius <fonsan@gmail.com>
2011-02-10 10:06:22 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EagerFail::Application.config.secret_token = '7c04c127c6cffe10c1b3fc9f3104388bd59727fff0dbb8b2573e66d1f25d57758359b1effa30a09ce3a9e0ec988dbee1a2008813ee8b2d30cb1d8c19acadd7e4'
